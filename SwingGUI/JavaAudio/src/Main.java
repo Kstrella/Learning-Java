57375559d9df08f2ea36724bb4bd657da19de349
Kstrella <90272570+Kstrella@users.noreply.github.com>
2022-02-20 16:40:13 -0500

    edit to java audio

--- SwingGUI/JavaAudio/src/Main.java
+++ SwingGUI/JavaAudio/src/Main.java
@@ -1,4 +1,4 @@
-/*
+p/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -11,9 +11,6 @@ import java.util.Scanner;
 import javax.sound.sampled.*;
 
 
-
-//703083838
-//Sweetcorn38
 
 /**
  *
